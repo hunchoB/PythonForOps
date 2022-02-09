9d9c9d945d6354b798078ed0b4147cd1289d5bf9
hunchob <alex.bobkov98@mail.ru>
2022-02-09 22:34:01 +0300

    Cycle

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 if __name__ == '__main__':
-    # language = "Python"
-    # position = "Ops"
-    # print(f"Изучить {language} " , end="")
-    # if "o" in language.lower() and not "g" in language:
-    #     print("намного проще", end="")
-    # else:
-    #     print("проще", end=" ")
-    # print(", чем сопромат")
-    branch_name = input()
-    is_test_completed = int(input())
-    coverage_count = int(input())
-    approve_count = int(input())
-    is_test_coverage_ok = is_test_completed and coverage_count > 5
-    is_test_approve = is_test_completed and coverage_count <= 5 and approve_count > 3
+    rps_values = (5081, '17184', 10968, 9666, '9102', 12321, '10617', 11633, 5035, 9554, '10424', 9378, '8577', '11602', 14116, '8066', '11977', '8572', 9685, 11062, '10561', '17820', 16637, 5890, 17180, '17511', '13203', 13303, '7330', 7186, '10213', '8063', '12283', 15564, 17664, '8996', '12179', '13657', 15817, '16187', '6381', 8409, '5177', 17357, '10814', 6679, 12241, '6556', 12913, 16454, '17589', 5292, '13639', '7335', '11531', '14346', 7493, 15850, '12791', 11288)
+    sum_of_rps_values = 0
+    rps_values_int = ()
 
-    if branch_name.lower() in ("development", "staging"):
-        if is_test_coverage_ok or is_test_approve:
-            print(f"Внимание! Код из {branch_name} отправлен в релиз!")
-        else:
-            print(f"Код из {branch_name} с результатами тесты: {is_test_completed}, coverage: {coverage_count}, approve: {approve_count} в релиз не попадает.")
-    else:
-        print(f"В ветке {branch_name} непроверенный код, пропускаем")
+    for values in rps_values:
+        values = int(values)
+        rps_values_int += (values,)
+        sum_of_rps_values += values
+        
+    tur = sorted(rps_values_int)    
+    quotient, remainder = divmod(len(rps_values_int), 2)
+    median = tur[quotient] if remainder else sum(tur[quotient - 1:quotient + 1]) / 2
+    
+    average_rps = sum_of_rps_values / len(rps_values_int)
+    print(average_rps, median)
+    # print(quotient, remainder)
+
