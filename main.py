3463bcee71b573fe5f2b2e073315339bec8b537d
hunchob <alex.bobkov98@mail.ru>
2022-02-11 00:05:07 +0300

    Functions and code into procedural paradigm

--- main.py
+++ main.py
@@ -1,28 +1,7 @@
-if __name__ == '__main__':
-    rps_values = [5081, '17184', 10968, 9666, '9102', 12321, '10617', 11633, 5035, 9554, '10424', 9378, '8577', '11602', 14116, 
-                  '8066', '11977', '8572', 9685, 11062, '10561', '17820', 16637, 5890, 17180, '17511', '13203', 13303, '7330', 7186, '10213', '8063', '12283', 
-                  15564, 17664, '8996', '12179', '13657', 15817, 15817, 15817, 15817, 15817, 15817,15817, 15817,'16187', '6381', 8409, '5177', 17357, '10814', 6679, 12241, '6556', 12913, 16454, '17589', 5292, '13639', '7335', 
-                  '11531', '14346', 7493, 15850, '12791', 11288]
-    # sum_of_rps_values = 0
-    rps_values_int = []
-    
-    # while True:
-    #     print("Input data:")
-    #     input_data = input()
-    #     if input_data.isdigit():
-    #         rps_values.append(int(input_data))
-    #     elif ";" in input_data:
-    #         rps_values.extend(map(int, input_data.split(";")))
-    #     elif input_data == "":
-    #         break
-    #     else:
-    #         print("Not correct data")
-            
-    rps_values_int.extend(map(int, rps_values))
-    
+def calculate_frequency(rps_values):
     info_frequency = {}
     
-    for values in rps_values_int:
+    for values in rps_values:
         if values in info_frequency:
             continue
         else:
@@ -30,27 +9,70 @@ if __name__ == '__main__':
             
     for keys in info_frequency:
         i = 0
-        for values in rps_values_int:
+        for values in rps_values:
             if keys == values:
                 i += 1
                 info_frequency[keys] = i
-    print(info_frequency)
+    return info_frequency
+
+def get_int_from_string(rps_values):
+    rps_values_int = []
+    rps_values_int.extend(map(int, rps_values))
+    return rps_values_int
+
+def calculate_average_of_rps(rps_values_int):
+    sum_of_rps_values = 0
     
-    # for values in rps_values_int:
-    #     sum_of_rps_values += values
+    for values in rps_values_int:
+        sum_of_rps_values += values
+        
+    average_rps = sum_of_rps_values / len(rps_values_int) 
+    return average_rps
+
+def input_new_data_into_list(rps_values):
+    while True:
+        print("Input data:")
+        input_data = input()
+        if input_data.isdigit():
+            rps_values.append(int(input_data))
+        elif ";" in input_data:
+            rps_values.extend(map(int, input_data.split(";")))
+        elif input_data == "":
+            break
+        else:
+            print("Not correct data")
+    return rps_values
+
+def calculate_median(rps_values_int):
+    rps_values_int.sort()    
+    quotient, remainder = divmod(len(rps_values_int), 2)
+    median = rps_values_int[quotient] if remainder else sum(rps_values_int[quotient - 1:quotient + 1]) / 2
+    return median
+
+def make_decision_about_loading(average_rps, median):
+    if average_rps - median > median * 0.25:
+        return average_rps, median, "Происходят скачки"
+    elif average_rps - median <= median * 0.25:
+        return average_rps, median, "Нагрузка стабильна"
+    else:
+        return average_rps, median, "Происходят снижения"
     
-    # average_rps = sum_of_rps_values / len(rps_values_int)          
+def main(rps_values):
+    get_int_from_string(rps_values)
+    updated_int_rps = input_new_data_into_list(get_int_from_string(rps_values))
+    calculate_frequency(updated_int_rps)
+    average_rps = calculate_average_of_rps(updated_int_rps)
+    median = calculate_median(updated_int_rps)
+    make_decision_about_loading(average_rps, median)
     
-    # rps_values_int.sort()    
-    # quotient, remainder = divmod(len(rps_values_int), 2)
-    # median = rps_values_int[quotient] if remainder else sum(rps_values_int[quotient - 1:quotient + 1]) / 2
+if __name__ == '__main__':
+    rps_values = [5081, 5081,5081,5081,5081,5081,5081,5081,5081,'17184', 10968, 9666, '9102', 12321, '10617', 11633, 5035, 9554, '10424', 9378, '8577', '11602', 14116, 
+    '8066', '11977', '8572', 9685, 11062, '10561', '17820', 16637, 5890, 17180, '17511', '13203', 13303, '7330', 7186, '10213', '8063', '12283', 
+    15564, 17664, '8996', '12179', '13657', 15817, 15817, 15817, 15817, 15817, 15817,15817, 15817,'16187', '6381', 8409, '5177', 17357, '10814', 6679, 12241, '6556', 12913, 16454, '17589', 5292, '13639', '7335', 
+    '11531', '14346', 7493, 15850, '12791', 11288]
     
-    # if average_rps - median > median * 0.25:
-    #     print(average_rps, median, "Происходят скачки")
-    # elif average_rps - median <= median * 0.25:
-    #     print(average_rps, median, "Нагрузка стабильна")
-    # else:
-    #     print(average_rps, median, "Происходят снижения")
+    main(rps_values)
+    
         
 # Second part 
     # rps_values_int_slice = rps_values_int.copy()
