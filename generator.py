409194e3fce0531e9ed6a3e862728fc04beec2e9
hunchob <alex.bobkov98@mail.ru>
2022-04-18 15:49:02 +0300

    Execute some commands on remote hosts

--- generator.py
+++ generator.py
@@ -1,11 +1,28 @@
-from ast import Pass
+def main(HOSTS, COMMANDS):
+    for host in HOSTS:
+        for command in COMMANDS:
+            if command != 'rm -rf /':
+                print(f"На хосте {host} была выполнена команда {command}")
+            else:
+                print(f"На хосте {host} была попытка выполнить команду {command} , но процесс выполнения был аварийно остановлен.")
+                break
+        else:
+            continue            
+        break
 
+def remove_space_symbols(COMMANDS):
+    command_clear = []        
+    for com in COMMANDS:
+        if com[0] == ' ':
+            command_clear.append(com.replace(' ', '', 1))
+        else:
+            command_clear.append(com)
+    return command_clear
 
-def main():
-    return print("hi")
-
-
-    
 if __name__ == '__main__':
+    ip_address = str(input().replace('[','').replace(']','').replace("'",'').replace(" ",''))
+    execute_command = str(input().replace('[','').replace(']','').replace("'",""))
+    HOSTS = list(map(str, ip_address.split(',')))
+    COMMANDS = list(map(str, execute_command.split(',')))
     
-    main()
+    main(HOSTS, remove_space_symbols(COMMANDS))
